0cc930c6150c1646a8ea3edb81ebade8969d753a
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2021-02-27 20:08:01 +0800

    Adds incomplete login, references, concrete documentlistener

---
+++ src/unnamedlibrary/unnamedReferences.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unnamedlibrary;
+
+/**
+ *
+ * @author fab07
+ */
+public class unnamedReferences {
+//    REFERENCE SLOT
+//    https://stackoverflow.com/questions/17132452/java-check-if-jtextfield-is-empty-or-not
+//    https://stackoverflow.com/questions/8721197/java-equivalent-to-c-sharp-textbox-textchanged-event
+//    https://stackoverflow.com/questions/10757555/java-documentlistener
+//    https://stackoverflow.com/questions/8721197/java-equivalent-to-c-sharp-textbox-textchanged-event
+}
