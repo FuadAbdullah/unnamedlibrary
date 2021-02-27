66cf5ccdff48db76fb5e5321c8bbedf125232f13
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2021-02-27 20:08:01 +0800

    Adds incomplete login, references, concrete documentlistener

---
+++ src/unnamedlibrary/unnamedDocumentListener.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unnamedlibrary;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ *
+ * @author fab07
+ */
+public class unnamedDocumentListener implements DocumentListener {
+    
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        // TO BE OVERRIDDEN
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        // TO BE OVERRIDDEN
+        
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        // TO BE OVERRIDDEN
+       
+    }
+}
